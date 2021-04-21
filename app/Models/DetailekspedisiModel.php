c11067711b6f173c3fec0d96a54d22de337021d4
SutrisDevPemula <56956940+SutrisDevPemula@users.noreply.github.com>
2021-04-22 06:13:24 +0800

    setup models done

--- app/Models/DetailekspedisiModel.php
+++ app/Models/DetailekspedisiModel.php
@@ -6,37 +6,18 @@ use CodeIgniter\Model;
 
 class DetailekspedisiModel extends Model
 {
-	protected $DBGroup              = 'default';
-	protected $table                = 'detailekspedisis';
-	protected $primaryKey           = 'id';
+	protected $table                = 'detailekspedisi';
+	protected $primaryKey           = 'id_detail_ekspedisi';
 	protected $useAutoIncrement     = true;
-	protected $insertID             = 0;
-	protected $returnType           = 'array';
-	protected $useSoftDelete        = false;
-	protected $protectFields        = true;
-	protected $allowedFields        = [];
+	protected $allowedFields        = [
+		'id_ekspedisi',
+		'id_motor',
+		'id_kerusakan',
+		'tanggal_ekspedisi',
+		'no_frame',
+		'no_mesin'
+	];
 
 	// Dates
 	protected $useTimestamps        = false;
-	protected $dateFormat           = 'datetime';
-	protected $createdField         = 'created_at';
-	protected $updatedField         = 'updated_at';
-	protected $deletedField         = 'deleted_at';
-
-	// Validation
-	protected $validationRules      = [];
-	protected $validationMessages   = [];
-	protected $skipValidation       = false;
-	protected $cleanValidationRules = true;
-
-	// Callbacks
-	protected $allowCallbacks       = true;
-	protected $beforeInsert         = [];
-	protected $afterInsert          = [];
-	protected $beforeUpdate         = [];
-	protected $afterUpdate          = [];
-	protected $beforeFind           = [];
-	protected $afterFind            = [];
-	protected $beforeDelete         = [];
-	protected $afterDelete          = [];
 }
--- app/Models/EkspedisiModel.php
+++ app/Models/EkspedisiModel.php
@@ -6,37 +6,21 @@ use CodeIgniter\Model;
 
 class EkspedisiModel extends Model
 {
-	protected $DBGroup              = 'default';
-	protected $table                = 'ekspedisis';
-	protected $primaryKey           = 'id';
+	protected $table                = 'ekspedisi';
+	protected $primaryKey           = 'id_ekspedisi';
 	protected $useAutoIncrement     = true;
-	protected $insertID             = 0;
-	protected $returnType           = 'array';
-	protected $useSoftDelete        = false;
-	protected $protectFields        = true;
-	protected $allowedFields        = [];
+	protected $allowedFields        = [
+		'no_polisi',
+		'nama_ekspedisi',
+		'no_do',
+		'no_sp_list',
+		'tgl_sp_list',
+		'tgl_do'
+	];
 
 	// Dates
 	protected $useTimestamps        = false;
-	protected $dateFormat           = 'datetime';
-	protected $createdField         = 'created_at';
-	protected $updatedField         = 'updated_at';
-	protected $deletedField         = 'deleted_at';
 
 	// Validation
 	protected $validationRules      = [];
-	protected $validationMessages   = [];
-	protected $skipValidation       = false;
-	protected $cleanValidationRules = true;
-
-	// Callbacks
-	protected $allowCallbacks       = true;
-	protected $beforeInsert         = [];
-	protected $afterInsert          = [];
-	protected $beforeUpdate         = [];
-	protected $afterUpdate          = [];
-	protected $beforeFind           = [];
-	protected $afterFind            = [];
-	protected $beforeDelete         = [];
-	protected $afterDelete          = [];
 }
